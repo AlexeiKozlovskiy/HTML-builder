c59a1d17f8bc5402847fd7d1be9b0bfbf525829f
AlexeiKozlovskiy <gekleberefin@gmail.com>
2022-05-21 14:31:19 +0300

    fix:change solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 const path = require('path');
 
 
-// fs.createWriteStream() сщздание потока в который можно записывать данные
+// fs.createWriteStream() - создание потока в который можно записывать данные
+// chunk - фрагмент данных
+
 const stream = fs.createReadStream(path.join(__dirname, '/text.txt'), 'utf-8');
 
 stream.on('data', function (chunk) {
     console.log(chunk);
   });
-  
