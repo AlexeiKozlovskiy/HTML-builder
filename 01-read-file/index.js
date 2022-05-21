db2435017485859cf13ac14c516870159e1eefca
AlexeiKozlovskiy <gekleberefin@gmail.com>
2022-05-21 14:26:46 +0300

    fix:change solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,12 @@
+// модули: взаимодуйствия с файловой системой, работы с путями
 const fs = require('fs');
 const path = require('path');
+
+
+// fs.createWriteStream() сщздание потока в который можно записывать данные
 const stream = fs.createReadStream(path.join(__dirname, '/text.txt'), 'utf-8');
 
 stream.on('data', function (chunk) {
     console.log(chunk);
   });
   
-stream.on('error', function (err) {
-    console.log('Ошибка чтения: ' + err.message);
-  });
